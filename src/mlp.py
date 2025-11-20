d8fe3dcd3ff7c935742f830e24f4039042f9d900
M.Rautenberg <m.rautenberg@th-ws-3450de02.theorie.physik.uni-muenchen.de>
2025-11-20 12:20:47 +0100

    moved monitoring of layer magnitudes in front of activation functions
    enabled automatic jax debugging when NaNs are encountered

--- src/mlp.py
+++ src/mlp.py
@@ -1,8 +1,10 @@
 # mlp.py
 from typing import Any, Callable, Tuple
 
+import jax
 import jax.numpy as jnp
 from flax import linen as nn
+jax.config.update("jax_debug_nans", True)
 
 from param_schemes import ParamScheme 
 
@@ -23,26 +25,26 @@ class MLP(nn.Module):
         # Input layer
         kinit = self.param_scheme.kernel_inits("in", self.kernel_dims, self.base_kernel_dims)
         x = nn.Dense(self.widths[0], kernel_init=kinit, use_bias=False, name="Dense_0")(x)
-        x = self.acts[0](x)
-        # print(x)
         if capture_layer_acts:
             layer_acts.append(jnp.abs(x).mean(axis=1))
+        x = self.acts[0](x)        
 
         # Hidden layers
         for i, w in enumerate(self.widths[1:-1], start=1):
             kinit = self.param_scheme.kernel_inits(f"Dense_{i-1}", self.kernel_dims, self.base_kernel_dims)
             x = nn.Dense(w, kernel_init=kinit, use_bias=False, name=f"Dense_{i}")(x)
-            x = self.acts[i](x)
             if capture_layer_acts:
                 layer_acts.append(jnp.abs(x).mean(axis=1))
-
+            x = self.acts[i](x)
+            
         # Output layer
         kinit = self.param_scheme.kernel_inits("out", self.kernel_dims, self.base_kernel_dims)
         y = nn.Dense(self.widths[-1], kernel_init=kinit, use_bias=False, name=f"Dense_{len(self.widths)-1}")(x)
-        y = self.acts[-1](y)
         if capture_layer_acts:
             layer_acts.append(jnp.abs(y).mean(axis=1))
             stacked_act = jnp.stack(layer_acts, axis=0).T
+        y = self.acts[-1](y)
+        
         if capture_layer_acts:
             return y, stacked_act
         return y
